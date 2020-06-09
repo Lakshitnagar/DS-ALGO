1d134a33819c6b97460a8454e78585886017cf86
Lakshit Nagar <lakshitnagar@gmail.com>
2020-06-09 10:44:57 +0530

    Read me for linked list

---
+++ ds/linkedlist/README.md
@@ -0,0 +1,6 @@
+# Linked List
+Here I have listed all the popular questions on linked lists with there solution in Java.
+
+## Questions
+00 - Implement Singly Linked List using Java class\
+P1 - Create and Print linked list. Insert and Delete data.
--- ds/linkedlist/p1/S1.java
+++ ds/linkedlist/p1/S1.java
@@ -7,12 +7,15 @@ public class S1 {
 
     public static void main(String args[]) {
 
+        // Creation of Linked List
         LinkedList linkedList = new LinkedList();
 
+        // Populating data
         linkedList.insert(1);
         linkedList.insert(2);
         linkedList.insert(3);
 
+        // Printing data
         linkedList.printLinkedList();
     }
 }
